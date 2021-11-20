5d34b80a0ecb0284fb02961af6bb57844d4c9b62
Łukasz Michalak <lupinummail@gmail.com>
2021-11-20 19:15:10 +0100

    Initial Commit

---
+++ OpenCV Projekt/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  OpenCV Projekt
+//
+//  Created by Łukasz Michalak on 20/11/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
